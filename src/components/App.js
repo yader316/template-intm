e9c0eff61341b71327f388e1ba33f5d93b0efd70
yader316 <yadtais316@gmail.com>
2021-08-18 21:52:10 -0600

    primer commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function About() {
+    return (
+        <div>
+            <h1 className='text-center'>About</h1>
+        </div>
+    )
+}
+
+export default About;
